b54a53c53ccce945194ea3e290ffc07dd4ae15ac
Vittesh Raghavan <vitteshraghavan@gmail.com>
2023-07-13 16:59:36 +0530

    Added 5th Patterns solution

---
+++ Patterns/5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int num,row,col;
+scanf("%d",&num);
+for(row=1;row<=num;row++,printf("\n"))
+{
+for(col=1;col<=row;col++)
+printf("*");
+}
+for(row=num;row>=1;row--,printf("\n"))
+{
+for(col=1;col<=row;col++)
+printf("*");
+}
+return 0;
+}
