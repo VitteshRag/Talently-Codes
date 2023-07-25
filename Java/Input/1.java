108ef292f836839bf04c41f9d5d34d1fb79bd38a
Vittesh Raghavan <vitteshraghavan@gmail.com>
2023-07-25 14:41:52 +0530

    Added Java solution

---
+++ Java/Input/1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class main
+{
+      public static void main(String[] args)
+       {
+            Scanner sc=new Scanner(System.in);
+            long a=sc.nextLong();
+            long b=sc.nextLong();
+            System.out.println("Sum of \\"+a+" and \\"+b+" is "+(a+b));
+        }
+}
